5affd73d358670dda3a984f9362ece95fbf62c2b
AbobakrSaeed <abobakrsaeed3@gmail.com>
2025-07-06 15:32:39 +0530

    configure mkcert to run the client server on https , secure certificate

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// use mkcert to generate a local SSL certificate
+// This is useful for development environments that require HTTPS
+//## npm install vite-plugin-mkcert --save-dev
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mkcert()],
+});
